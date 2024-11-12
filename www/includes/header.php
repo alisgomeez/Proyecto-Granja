9f85e9f5f078ffefd67adc59fb97128054b39038
alis <fa.gomezgomez@ugto.mx>
2024-11-12 01:49:38 -0600

    se agregó menu en la barra

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,12 +1,38 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-c9eh+N37ea4YU27lK0ZxFRaFqgmrJ3hH6eZE5+1BvabDyIuAc5pysObkpluCNCI6" crossorigin="anonymous">
+    <title>Ganadería Gómez</title>
+</head>
+<body>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="inicio.php">Ganadería Gómez</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="inicio.php">Ganadería Gómez</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="animales.php">Animales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="alimentar.php">Alimentar y Vacunar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="almacen.php">Almacén</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="caja.php">Caja</a>
+                </li>
+            </ul>     
+</form>
+
+        </div>
     </div>
-  </div>
 </nav>
