1e56d1e2d893fb473e9e9390552ae376c4552a7a
alis <fa.gomezgomez@ugto.mx>
2024-11-11 20:25:12 -0600

    menu y páginas creadas

---
+++ www/animales.php
@@ -0,0 +1,76 @@
+<?php include("includes/header.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-c9eh+N37ea4YU27lK0ZxFRaFqgmrJ3hH6eZE5+1BvabDyIuAc5pysObkpluCNCI6" crossorigin="anonymous">
+    <title>Menú</title>
+</head>
+<body>
+
+<div class="container mt-4 text-center">
+    <div class="row justify-content-center d-flex align-items-center">
+        <div class="col-md-4">
+            <div class="btn-container">
+                <a href="puerinfo.php" class="btn btn-lg btn-secondary btn-white-border">
+                    <img src="../images/lotes.jpg" alt="Lotes" class="img-fluid rounded">
+                </a>
+                <div class="btn-name">Mostrar Puercas</div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="btn-container">
+                <a href="camadasinfo.php" class="btn btn-lg btn-secondary btn-white-border">
+                    <img src="../images/lotes.jpg" alt="Lotes" class="img-fluid rounded">
+                </a>
+                <div class="btn-name">Mostrar Camadas</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center d-flex align-items-center">
+        <!-- Dejar por si se agrega "VENDER PUERCA" --> 
+        <!-- <div class="col-md-4">
+            <div class="btn-container">
+                <a href="comprarlote.php" class="btn btn-sm btn-secondary btn-white-border">
+                    <img src="images/comprar.jpg" alt="Comprar Lote" class="img-fluid rounded">
+                </a>
+                <div class="btn-name">Comprar Lote</div>
+            </div>
+        </div>  -->
+
+        <div class="col-md-4">
+            <div class="btn-container">
+                <a href="vender.php" class="btn btn-sm btn-secondary btn-white-border">
+                    <img src="images/vender.png" alt="Vender Camada" class="img-fluid rounded">
+                </a>
+                <div class="btn-name">Vender Camada</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .btn-white-border {
+        background-color: #ffffff; /* Fondo blanco */
+        border: 2px solid #6c757d; /* Borde gris */
+        max-width: 200px; /* Ancho máximo */
+        margin: 0 auto; /* Centrar */
+    }
+
+    .btn-container {
+        margin-bottom: 15px;
+    }
+
+    .btn-name {
+        margin-top: 5px;
+    }
+</style>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-Y7xk1Bjc8F9zJxKtJcC5EPsbFVf0jcZ5z5nFf5r5l5uue6bF5f5u5z5z5z5z5z5z5z" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ www/camadasinfo.php
@@ -0,0 +1,36 @@
+<?php include("includes/header.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Mostrar Camadas</title>
+</head>
+<body>
+
+<div class="container mt-5">
+    <h2>Seleccionar Lote</h2>
+    
+    <?php
+    $servername = "database";
+    $username = "root";
+    $password = "root";
+    $dbname = "Granja";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id_camada, id_corral FROM Camadas";
+    $result = $conn->query($sql);
+    ?>
+</div>
+
+<!-- Bootstrap Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-bz3htznfnCJUiN+ouzWEhA0J6i/DOTt8Y5FzhKG6z13MiWBKRl0pMb7OoBydSMIk" crossorigin="anonymous"></script>
+</body>
+</html>
