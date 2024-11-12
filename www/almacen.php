7fecb389815fdeee9df81acca2d993fb54e8c76a
alis <fa.gomezgomez@ugto.mx>
2024-11-12 01:52:30 -0600

    se creo pagina para ver almacen y opciones

---
+++ www/almacen.php
@@ -0,0 +1,53 @@
+<?php include("includes/header.php");?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-c9eh+N37ea4YU27lK0ZxFRaFqgmrJ3hH6eZE5+1BvabDyIuAc5pysObkpluCNCI6" crossorigin="anonymous">
+    <title>Ganadería Gómez</title>
+</head>
+
+<body>
+
+
+<div class="container mt-4 text-center">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="btn-container">
+                <a href="alimentos.php" class="btn btn-lg btn-secondary btn-white-border">
+                    <img src="../images/alimentoic.png" alt="Alimentos" class="img-fluid rounded" style="width: 220px;">
+                </a>
+                <div class="btn-name">Alimentos</div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="btn-container">
+                <a href="medicamentos.php" class="btn btn-lg btn-secondary btn-white-border">
+                    <img src="../images/vacunaic.png" alt="Vacuna" class="img-fluid rounded" style="width: 220px;">
+                </a>
+                <div class="btn-name">Medicamentos</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .btn-white-border {
+        background-color: #ffffff; 
+        border: 2px solid #6c757d; 
+    }
+
+    .btn-container {
+        margin-bottom: 15px;
+    }
+</style>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-Y7xk1Bjc8F9zJxKtJcC5EPsbFVf0jcZ5z5nFf5r5l5uue6bF5f5u5z5z5z5z5z5z5z" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
